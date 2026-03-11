673abcd2141aeda77b1faf2a468fd266f2251956
balqaasem <jbashir52@gmail.com>
2026-03-11 17:41:28 +0800

    feat(devops): port setup and tooling scripts from Aleph Zero

---
+++ repos/setheum/scripts/run_local_e2e_pipeline.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+cargo build --release -p setheum-node
+docker build --tag setheum-node:latest -f ./docker/Dockerfile .
+
+cargo build --release -p chain-bootstrapper --features short_session
+docker build --tag chain-bootstrapper:latest -f ./bin/chain-bootstrapper/Dockerfile .
+
+# run the chain and the tests in two separate tmux windows
+tmux new-session -d -s aleph0 './.github/scripts/run_consensus.sh';
+tmux new-window -t "aleph0:1";
+tmux send-keys -t "aleph0:1" './scripts/run_e2e.sh' Enter;
+
+tmux a;
+
+exit $?
